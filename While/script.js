d8a74f3a4fff1baf296e32a057f37fad9997d9cf
PolatDeu <ahmetpolat@mein.gmx>
2021-06-28 19:45:44 +0200

    js began

---
+++ While/script.js
@@ -0,0 +1,23 @@
+//  let i=0, sum=0;
+//  while (i<6) {
+//      sum=sum+i;
+//      i++;
+//  }
+//  console.log(sum);
+//  console.log(`Total is : ${sum}`);
+
+let i=5, sum=0;
+do{
+    sum+=i;
+}while (i<5); 
+console.log(sum);
+//önce ciktiyi aliyor sonra sarta bakiyor sagliyormu diye
+
+for (let a = 0; a<5; a++) {
+    document.write("a",a)
+    console.log(a)  
+}
+for (let b = 3; b<15; b++) {
+    document.write("b",b)
+    console.log(b)  
+}
