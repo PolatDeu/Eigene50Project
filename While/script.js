f31768f1771d79f13923c49ac0ab2b8910ee2886
PolatDeu <ahmetpolat@mein.gmx>
2021-06-28 22:38:13 +0200

    Ein gutes opacity js wurde gemacht

--- While/script.js
+++ While/script.js
@@ -21,3 +21,41 @@ for (let b = 3; b<15; b++) {
     document.write("b",b)
     console.log(b)  
 }
+// let counter=0;
+// for (let c = 1; c<50; c++) {
+//     // document.write("c",c)
+//     counter = counter +c
+// }
+// console.log("von 1 bis 50 plus die Zahlen", counter);
+let counter=0;
+// for (d = 0; d<50; d++) {
+//      // document.write("c",c)
+//     i % 10===0 ? (counter+=i) : null;
+// }
+// console.log("von 1 bis 50 10 division null die Zahlen", counter);
+
+// let brand= "Clarusway"
+
+// for(let c=0; c<brand.length; c++){
+//     console.log(i+1, brand[i])
+// }
+
+for(let i=0; i<=10; i++){
+    for(j=0; j<=10; j++){
+        document.write(` ${i}*${j} = ${j*i}`,"<br/>");
+    }
+}
+
+const number = [13,45,87,98,98,54];
+let result=0;
+for(let k=0; k<number.length; k++){
+    result+=number[i];
+}
+console.log(result);
+
+let x= prompt("Enter a Nummer");
+result=1;
+for(i=1; i<=x; i++){
+    result=result*i;
+}
+console.log(result);
